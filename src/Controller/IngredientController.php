adc382846bab226940f8757956d5d164762c4e85
claudemarjean <marjean.8888@gmail.com>
2023-02-17 07:34:38 +0100

    episode 8♻️♻️ 26:12

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -12,7 +12,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IngredientController extends AbstractController
 {
-    #[Route('/ingredient', name: 'app_ingredient')]
+    /** This function display all ingredients  
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'app_ingredient', methods: ['GET'])]
     public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
         $ingredients = $paginator->paginate(
