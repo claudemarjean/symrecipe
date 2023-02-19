c7bfa2258d10617cd95b715336f5cc20fe8ce9b2
claudemarjean <marjean.8888@gmail.com>
2023-02-20 00:23:02 +0100

    Ajout 🚀🚀 51:53 episode 8

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Ingredient;
 use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,7 +21,7 @@ class IngredientController extends AbstractController
      * @param Request $request
      * @return Response
      */
-    #[Route('/ingredient', name: 'app_ingredient', methods: ['GET'])]
+    #[Route('/ingredient', name: 'ingredient_index', methods: ['GET'])]
     public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
         $ingredients = $paginator->paginate(
@@ -34,10 +35,26 @@ class IngredientController extends AbstractController
     }
 
     #[Route('/ingredient/nouveau', 'ingredient.new', methods: ['GET', 'POST'])]
-    public function new() : Response
+    public function new(
+        Request $request,
+        EntityManagerInterface $manager
+        ) : Response
     {
         $ingredient = new Ingredient();
         $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $ingredient = $form->getData();
+
+            $manager->persist($ingredient);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'votre ingrédient a été créé avec succès !'
+            );
+        }
 
         return $this->render('pages/ingredient/new.html.twig',[
             'form' => $form->createView()
