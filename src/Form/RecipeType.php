c1164a3d9461dd21a9f8c4cc54be8470e421c4db
claudemarjean <marjean.8888@gmail.com>
2023-03-01 07:25:43 +0100

    44:00💬💬💬💬💬💬💬

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -3,8 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Recipe;
+use App\Entity\Ingredient;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -123,8 +126,26 @@ class RecipeType extends AbstractType
                         new Assert\NotNull()
                 ]
             ])
-            ->add('ingredients')
-            ->add('submit', SubmitType::class);
+            ->add('ingredients',EntityType::class,[
+                'class' => Ingredient::class,
+                'query_builder' => function (EntityRepository $r) {
+                    return $r->createQueryBuilder('i')
+                        ->orderBy('i.name', 'ASC');
+                },
+                'label' => 'Les ingrédients',
+                'label_attr'=>[
+                    'class' => 'form-label mt-4'
+                ],
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true
+            ])
+            ->add('submit', SubmitType::class,[
+                'attr'=>[
+                    'class' => 'btn btn-primary mt-4'
+                    ],
+                'label' => 'Créer ma recette'
+                ]);
         ;
     }
 
